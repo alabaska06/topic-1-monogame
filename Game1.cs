9adca584c7e89803a093b7e64dbbd4dfa0f0bd4c
Alanna Smith <smitalan240@gmail.com>
2024-03-28 13:12:12 -0400

    I added a bunch of images, next step is to figure out how to randomly choose a background.

--- Game1.cs
+++ Game1.cs
@@ -6,7 +6,16 @@ namespace topic_1_monogame
 {
     public class Game1 : Game
     {
+        Texture2D starsTexture;
+        Texture2D purplespaceTexture;
         Texture2D planetTexture;
+        Texture2D deathstarsTexture;
+        Texture2D deathbeamTexture;
+        Texture2D boomTexture;
+        Texture2D alienHDTexture;
+
+        MouseState mouseState;//MOUSE
+
 
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
@@ -28,13 +37,22 @@ namespace topic_1_monogame
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            starsTexture = Content.Load<Texture2D>("stars");
+            purplespaceTexture = Content.Load<Texture2D>("purplespace");
             planetTexture = Content.Load<Texture2D>("planet");
+            deathstarsTexture = Content.Load<Texture2D>("deathstars");
+            deathbeamTexture = Content.Load<Texture2D>("deathbeam");
+            boomTexture = Content.Load<Texture2D>("boom");
+            alienHDTexture = Content.Load<Texture2D>("alienHD");
 
             // TODO: use this.Content to load your game content here
         }
 
         protected override void Update(GameTime gameTime)
         {
+            mouseState = Mouse.GetState();//MOUSE
+            Window.Title = mouseState.X + ", " + mouseState.Y;//MOUSE
+
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
@@ -49,7 +67,12 @@ namespace topic_1_monogame
 
 
             _spriteBatch.Begin();
-            _spriteBatch.Draw(planetTexture, new Rectangle(300, 150, 100, 100), Color.White);
+            _spriteBatch.Draw(starsTexture, new Vector2(0, 0), Color.White);
+            _spriteBatch.Draw(planetTexture, new Rectangle(150, 120, 300, 300), Color.White);
+            _spriteBatch.Draw(deathstarsTexture, new Vector2(500, 25), Color.White);
+            _spriteBatch.Draw(deathbeamTexture, new Vector2(255, 120), Color.White);
+            _spriteBatch.Draw(boomTexture, new Rectangle(170, 120, 150, 150), Color.White);
+            _spriteBatch.Draw(alienHDTexture, new Rectangle(325, 10, 50, 130), Color.White);
             _spriteBatch.End();
 
             // TODO: Add your drawing code here
